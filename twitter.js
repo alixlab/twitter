cfba64799ec654f9948916d094aa370e003ad5fd
alixlab <alicelabarca@gmail.com>
2019-02-20 15:02:49 -0300

    Versão 0.0.3, incluindo um arquivo CSS

--- twitter.js
+++ twitter.js
@@ -1,14 +1,41 @@
-document.getElementById("btnTwitter").addEventListener("click", addTweet);
-
 let twitterText = document.querySelector("#twitterText");
 let tweetList = document.querySelector("#tweets");
+const maxChar = 140;
 
-function addTweet() {
+document.getElementById("btnTwitter").addEventListener("click", addTweet);
+document.getElementById("twitterText").addEventListener("input", enableBtn);
+    
+function enableBtn(event) {
+    if (twitterText.value === "") {
+        document.getElementById("btnTwitter").disabled = true;
+    } else {
+        document.getElementById("btnTwitter").disabled = false;    
+    }
+}
+
+function addTweet(event) {
     let newList = document.createElement('li');
 
     newList.textContent = twitterText.value;
     tweetList.appendChild(newList);   
-    twitterText.value = '';
+    twitterText.value = "";
+    document.getElementById("btnTwitter").disabled = true;
 }
 
+window.addEventListener("keyup", counterChar)
 
+function counterChar(event) {
+    let counter = document.getElementById("counter");
+    let countNum = maxChar - twitterText.value.length;
+    document.getElementById("counter").innerHTML = countNum;
+    
+    if (twitterText.value.length > 140) {
+        document.getElementById("btnTwitter").disabled = true;
+    } else if (countNum <= 20 && countNum > 10) {
+        counter.style.color = "orange";
+    } else if (countNum <= 10 && countNum >= 0) {
+        counter.style.color = "red";
+    } else {
+        counter.style.color = "black";
+    }
+}
